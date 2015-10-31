b3384e4fac8f9b544fe51a0e4e24266b2befc321
Deva Dixon <dixo84493@email.ccbcmd.edu>
2015-10-31 19:13:52 -0400

    Mostly working. Needs check for empty text field, some text edits, styled text.

--- FieldButtonFun/ViewController.swift
+++ FieldButtonFun/ViewController.swift
@@ -10,26 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var thePlace: UITextField!
     @IBOutlet weak var theVerb: UITextField!
     @IBOutlet weak var theNumber: UITextField!
-    @IBOutlet weak var theTemplate: UITextView!
     @IBOutlet weak var theStory: UITextView!
+    @IBOutlet weak var theName: UITextField!
+    
+    let template1 = "The CCBC <name>'s descended upon <place>.\nThey vowed to <verb> night and day, until all <number> pizzas were consumed. \n<place> would never be the same again."
+    
+    let template2 = "Hey! Mr. <name>, play <number> songs for me.\nI'm not sleepy, and there is\nno <place> I'm <verb>ing to."
+    
+    let template3 = "'Twas brillig, and the <number> toves\n\tdid gyre and <verb> in the <place>.\nAll mimsy were the <name>'s,\n\tand the mome raths outgrabe."
+    
+    var count = 1
     
     @IBAction func createStory(sender: AnyObject) {
-        theStory.text=theTemplate.text
-    theStory.text=theStory.text.stringByReplacingOccurrencesOfString("<place>", withString: thePlace.text! )
         
+    
+        
+        switch count {
+            case 1:
+                self.theStory.text=self.template1
+                self.count=self.count + 1
+                storyLabel.text = "Story: (1 of 3)"
+         
+            case 2:
+                self.theStory.text=self.template2
+                self.count = self.count + 1
+                storyLabel.text = "Story: (2 of 3)"
+            
+            case 3:
+                self.theStory.text = self.template3
+                self.count = 1
+                storyLabel.text = "Story: (3 of 3)"
+            
+        default:
+            self.count = 1
+            createStory(self)
+        }
+        
+        
+        theStory.text = theStory.text.stringByReplacingOccurrencesOfString("<name>", withString: theName.text!)
+        theStory.text=theStory.text.stringByReplacingOccurrencesOfString("<place>", withString: thePlace.text! )
         theStory.text=theStory.text.stringByReplacingOccurrencesOfString("<verb>", withString: theVerb.text!)
         theStory.text=theStory.text.stringByReplacingOccurrencesOfString("<number>", withString: theNumber.text!)
         
+        deleteText()
+    }
+    
+    func deleteText() {
+        thePlace.text = nil
+        theNumber.text = nil
+        theVerb.text = nil
+        theName.text = nil
     }
     
     @IBAction func hideKeyboard(sender: AnyObject) {
         thePlace.resignFirstResponder()
         theVerb.resignFirstResponder()
         theNumber.resignFirstResponder()
-        theTemplate.resignFirstResponder()
+        theName.resignFirstResponder()
     }
     
     override func viewDidLoad() {
